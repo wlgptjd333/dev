1055e2657507863814f5b9a8e742dbf35a076bb6
DESKTOP-761SQI1\user1 <wlgptjd333@gmail.com>
2026-07-16 10:52:15 +0900

    2교시

---
+++ python/prj01/test02.py
@@ -0,0 +1,6 @@
+# 입력
+x = input("당신의 나이는?")
+print(x)
+int(x) # x가 아니라 그 안에 담긴 내용물인 숫자가 int로 형변환
+x = int(x) # 입력값을 int로 형변환하고 x에 저장
+print(type(x))
---
+++ python/prj01/test03.py
@@ -0,0 +1,7 @@
+# 형변환 (type casting)
+
+x = 0
+print(x)
+x = bool(x)
+print(type(x))
+print(x)
