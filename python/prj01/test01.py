0ad341343d38476427ecb336b75308aada48f04c
DESKTOP-761SQI1\user1 <wlgptjd333@gmail.com>
2026-07-16 10:21:48 +0900

    깃이그노어 설정

--- python/prj01/test01.py
+++ python/prj01/test01.py
@@ -1,4 +1,28 @@
 # 변수란?
+'''
+변수명: 숫자, 몇몇(_)제외한 특수문자로 시작 X
+'''
 
-x = "true"
-print(x)
+# 타입(자료형) 4가지
+'''
+int     : 정수
+float   : 실수
+str     : 문자열(파이썬은 문자와 문자열 구분 X, 아무 따옴표 필요)
+bool    : 논리값(True/False)
+'''
+
+age = 18
+print(age)
+print(type(age))
+
+height = 187.3
+print(height)
+print(type(height))
+
+이름 = "홍길동"
+print(이름)
+print(type(이름))
+
+is_adult = True
+print(is_adult)
+print(type(is_adult))
